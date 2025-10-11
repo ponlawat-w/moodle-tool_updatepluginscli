719a0e3ce695ee4397b5692358ad057cb2ea1faa
Ponlawat W. <ponlawat_w@outlook.co.th>
2025-10-11 13:50:07 +0700

    Bumped version.php

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tool_updatepluginscli';
-$plugin->release = '1.0.2';
-$plugin->version = 2025100700;
+$plugin->release = '1.0.3';
+$plugin->version = 2025101100;
 $plugin->requires = 2024100700;
 $plugin->maturity = MATURITY_STABLE;
